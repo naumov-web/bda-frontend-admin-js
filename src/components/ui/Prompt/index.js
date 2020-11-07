f9794ac60d05d55217e74ca1ae9296251f90bda6
System Administrator <root@MacBook-Pro-Konstantin-2.local>
2020-11-07 22:46:15 +0300

    Create graph form

---
+++ src/components/ui/Prompt/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+// Styles
+import './styles.sass';
+
+export default ({ text }) => {
+  return <div className="prompt">{ text }</div>;
+};
--- src/store/rawData/actionCreators.js
+++ src/store/rawData/actionCreators.js
@@ -1,4 +1,10 @@
-import { SET_RAW_DATA, SET_RAW_DATA_ITEM, SET_IS_LOADING, SET_PAGINATION } from './actionTypes';
+import { 
+  SET_RAW_DATA, 
+  SET_RAW_DATA_ITEM, 
+  SET_IS_LOADING, 
+  SET_PAGINATION, 
+  SET_RAW_DATA_ITEM_EXAMPLE
+} from './actionTypes';
 
 export const createSetRawDataAction = (rawData, count) => ({
   type: SET_RAW_DATA,
@@ -19,4 +25,9 @@ export const createSetPaginationAction = (params) => ({
 export const createSetRawDataItemAction = (rawDataItem) => ({
   type: SET_RAW_DATA_ITEM,
   rawDataItem
+});
+
+export const createSetRawDataItemExampleAction = (rawDataItem) => ({
+  type: SET_RAW_DATA_ITEM_EXAMPLE,
+  rawDataItemExample: rawDataItem
 });
